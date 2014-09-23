182f204cd861248c2b2e3eba19e0018fc00e1c0a
Francesco Caruso <guernika79@gmail.com>
2014-09-23 15:17:39 +0200

    removed other piece of code

--- 04_ImageBlur/imageBlur.cu
+++ 04_ImageBlur/imageBlur.cu
@@ -19,13 +19,13 @@ unsigned char *d_img  = NULL;
 #define BLOCK_HEIGHT	32
 
 template<unsigned short RADIUS >
-__global__ void kRadialBlur( unsigned char* img, unsigned width, unsigned height, size_t pitch)
+__global__ void kRadialBlur( ... )
 {
 
 			
 }
 
-__global__ void kBlur(unsigned char* img, unsigned width, unsigned height, size_t pitch)
+__global__ void kBlur( ... )
 {
 
 
@@ -41,20 +41,17 @@ int main(int argc, char* argv[])
 	// fill GPU 
 	unsigned char* d_img = NULL;
 	size_t pitch;
-	cudaMallocPitch( (void**) &d_img, &pitch, width*sizeof(unsigned char), height );
-	cudaMemcpy2D( d_img, pitch*sizeof(unsigned char), 
-			h_img, width*sizeof(unsigned char), width*sizeof(unsigned char), height, 
-			cudaMemcpyHostToDevice );
+	cudaMallocPitch( .... );
+	cudaMemcpy2D( .... );
 
 	// process image
-	dim3 dGrid(width / BLOCK_WIDTH, height / BLOCK_HEIGHT);
-	dim3 dBlock(BLOCK_WIDTH, BLOCK_HEIGHT);
-	kRadialBlur<4> <<< dGrid, dBlock >>> (d_img, width, height, pitch );
+	dim3 dGrid( ... );
+	dim3 dBlock( ... );
+	kBlur<<<....>>> ( .... )
+	kRadialBlur<4> <<< .... >>> ( .... );
 	cudaThreadSynchronize();
 	// save image
-	cudaMemcpy2D( h_img, width*sizeof(unsigned char), 
-		d_img, pitch*sizeof(unsigned char), width*sizeof(unsigned char), height,
-		cudaMemcpyDeviceToHost );
+	cudaMemcpy2D( .... );
 	sdkSavePGM("blurred.ppm", h_img, width, height );
 
 	// free memory
